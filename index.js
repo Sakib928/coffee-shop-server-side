438948523d93420a74fb763a618389038c36690a
Sakib928 <sakibgscian@gmail.com>
2024-04-26 19:22:18 +0600

    added login logout

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ async function run() {
         await client.connect();
 
         const coffeeCollection = client.db("coffeeDB").collection("coffee");
+        const userCollection = client.db('coffeeDB').collection('user');
 
         app.post('/coffee', async (req, res) => {
             const newCoffee = req.body;
@@ -76,6 +77,40 @@ async function run() {
             res.send(result);
         })
 
+
+        // user related apis
+        app.get('/user', async (req, res) => {
+            const cursor = userCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/user', async (req, res) => {
+            const user = req.body;
+            console.log(user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        })
+
+        app.patch('/user', async (req, res) => {
+            const user = req.body;
+            const filter = { email: user.email };
+            const updateDoc = {
+                $set: {
+                    lastLogin: user.lastLoggedAt
+                }
+            }
+            const result = await userCollection.updateOne(filter, updateDoc);
+            res.send(result);
+
+        })
+
+        app.delete('/user/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await userCollection.deleteOne(query);
+            res.send(result)
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
